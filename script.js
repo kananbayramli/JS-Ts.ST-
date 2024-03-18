84171bc8105e656bfacf40910df4f2d62cc19034
kananbayramli <bayramli-kenan@mail.ru>
2024-03-18 13:29:35 +0400

    First Commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var kecid = 50;
+
+var stu1Name = "Kenan Bayram";
+var stu1Birth = "1995";
+var noteMath = 70;
+var noteBio = 70;
+var noteHistory = 80;
+var stu1Yas = 2024 - Number(stu1Birth);
+var stu1Average = (noteBio + noteMath + noteHistory) / 3;
+var kecib = stu1Average >= 50;
+
+console.log("Yas: "+stu1Yas+" Ortalama: " + stu1Average + " Kecib: "+ kecib);
+
+
+
+var stu2Name = "Kamal Bayram";
+var stu2Birth = "1997";
+var noteMath1 = 40;
+var noteBio1 = 40;
+var noteHistory1 = 50;
+var stu2Yas = 2024 - Number(stu2Birth);
+var stu2Average = (noteMath1 + noteBio1 + noteHistory1) / 3;
+var kecib1 = stu2Average >= 50;
+console.log("Yas: "+stu2Yas+" Ortalama: " + stu2Average + " Kecib: "+ kecib1);
+
+
