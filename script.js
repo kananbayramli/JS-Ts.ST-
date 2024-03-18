06d56b33b366cc2184c91f7ae1a8db31dca819ab
kananbayramli <bayramli-kenan@mail.ru>
2024-03-18 21:32:31 +0400

    Javascript Object Notation

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // var kecid = 50;
 
 
+
 // var stu1Name = "Kenan Bayram";
 // var stu1Birth = "1995";
 // var noteMath = 70;
@@ -63,27 +64,116 @@
 
 // console.log(mesaj);
 
-let url = "https://www.kananbayram.com";
-let kursAdi = "Full Web Sayt Kursu";
+// let url = "https://www.kananbayram.com";
+// let kursAdi = "Full Web Sayt Kursu";
 
-// url uzunlugunu tapin.
-let uzunluq = url.length;
-console.log(uzunluq);
+// // url uzunlugunu tapin.
+// let uzunluq = url.length;
+// console.log(uzunluq);
 
-//kursadi nece sozden ibaretdir
-let list = kursAdi.split(" ");
-console.log(list.length);
+// //kursadi nece sozden ibaretdir
+// let list = kursAdi.split(" ");
+// console.log(list.length);
 
-//url https-lemi bashliyir?
-let start = url.substring(0,5);
-let yoxla = start === "https"
-console.log(yoxla);
+// //url https-lemi bashliyir? //startwithsiz
+// let start = url.substring(0,5); 
+// let yoxla = start === "https"
+// console.log(yoxla);
 
-// kurs adi icerisinde "HTML" sozu varmi?
-let yoxlaHtml = kursAdi.includes("HTML");
-console.log(yoxlaHtml);
+// // kurs adi icerisinde "HTML" sozu varmi?
+// let yoxlaHtml = kursAdi.includes("HTML");
+// console.log(yoxlaHtml);
 
-//https://www.kananbayram.com/full-web bunu duzeldin.
-var urlHelper =`${list[0].toLowerCase()}-${list[1].toLowerCase()}`
-var baseUrl = `${url}/${urlHelper}`;
-console.log(baseUrl);
+// //https://www.kananbayram.com/full-web bunu duzeldin.
+// var urlHelper =`${list[0].toLowerCase()}-${list[1].toLowerCase()}`
+// var baseUrl = `${url}/${urlHelper}`;
+// console.log(baseUrl);
+
+
+//************Array*************
+// 1- Alma, Armud , Banan , Ciyelek elementlerine sahib list yaradin
+// var fruits = ["Alma", "Armud", "Banan", "Ciyelek"];
+
+// // 2- listin nece elementi var ?
+// console.log(fruits.length);
+
+// // 3- Listin ilk ve son elementleri nelerdir?
+// sonelement = fruits.pop(); //son elementi listden silir ve ozunde saxlayir
+// ilkelement = fruits.shift(); //ilk elementi silir ve ozunde saxlayir
+// console.log(`Son element ${sonelement}`);
+// console.log(`Ilk element ${ilkelement}`);
+
+// console.log(fruits[0]); //ilk
+// console.log(fruits[fruits.length-1]); //son
+
+
+// // 4- Alma listin bir elementidirmi?
+// var yoxla = fruits.includes("Alma");
+// console.log(yoxla);
+
+// // 5- Gilas meyvesini liste elave edin
+// fruits[fruits.length] = "Gilas";
+// fruits.push("Ananas"); //elementi sona elave edir
+// fruits.unshift("Heyva"); //listin evveline elementi elave edir
+
+// console.log(fruits);
+
+
+// Telebelrin adini yashini ve ortalamasini gosterin
+
+// var std1 = ["Kenan", "Bayram", 1995, [70, 80, 90]];
+
+// console.log(`Name: ${std1[0]} \nSurname: ${std1[1]}\nYas:${(new Date()).getFullYear() - std1[2]}\nOrtalama: ${((std1[3][0]) + (std1[3][1]) + (std1[3][2])) / 3}`);
+
+
+
+
+/* 
+    1- Sifarish bilgilerini object icerinde saxlayin;
+    2- Her sifariwin vergi daxil qiymetini hesablayin ; (vergi 18%)
+    3- Butun sifariwlerin vergi daxil qiymetini hesablayin;
+*/
+
+let sifariw1 = {
+    "sif_id" : 102,
+    "sif_tarix": "30.12.2024",
+    "odeme" : "kart",
+    "kargo_adres":{
+        "Kuce" : "Anashkin",
+        "Rayon" : "Sabunchu",
+        "Sheher" : "Baki"
+    },
+    "products" : [
+        {
+            "pro_id" : 6,
+            "pro_Name" : "Samsung s 26",
+            "pro_price" : 2300
+        }
+    ]
+}
+
+var sifVergiDaxil = (sifariw1.products[0].pro_price * 1.18)
+
+let sifariw2 = {
+    "sif_id" : 103,
+    "sif_tarix": "31.12.2024",
+    "odeme" : "kart",
+    "kargo_adres":{
+        "Kuce" : "Anashkin",
+        "Rayon" : "Sabunchu",
+        "Sheher" : "Baki"
+    },
+    "products" : [
+        {
+            "pro_id" : 7,
+            "pro_Name" : "IPhone 13 Pro Max",
+            "pro_price" : 3300
+        }
+    ]
+}
+
+var sifVergiDaxi2 = (sifariw2.products[0].pro_price * 1.18)
+console.log(sifVergiDaxi2);
+
+var totalSum = sifVergiDaxil + sifVergiDaxi2;
+console.log(`Total Sum : ${totalSum}`)
