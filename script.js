0c2a9a41ee0ce0908b779ad58530a3043d4f53b1
kananbayramli <bayramli-kenan@mail.ru>
2024-03-18 15:08:29 +0400

    First Commit

--- script.js
+++ script.js
@@ -1,26 +1,55 @@
-var kecid = 50;
+// var kecid = 50;
 
-var stu1Name = "Kenan Bayram";
-var stu1Birth = "1995";
-var noteMath = 70;
-var noteBio = 70;
-var noteHistory = 80;
-var stu1Yas = 2024 - Number(stu1Birth);
-var stu1Average = (noteBio + noteMath + noteHistory) / 3;
-var kecib = stu1Average >= 50;
+// var stu1Name = "Kenan Bayram";
+// var stu1Birth = "1995";
+// var noteMath = 70;
+// var noteBio = 70;
+// var noteHistory = 80;
+// var stu1Yas = 2024 - Number(stu1Birth);
+// var stu1Average = (noteBio + noteMath + noteHistory) / 3;
+// var kecib = stu1Average >= 50;
 
-console.log("Yas: "+stu1Yas+" Ortalama: " + stu1Average + " Kecib: "+ kecib);
+// console.log("Yas: "+stu1Yas+" Ortalama: " + stu1Average + " Kecib: "+ kecib);
 
 
+// var stu2Name = "Kamal Bayram";
+// var stu2Birth = "1997";
+// var noteMath1 = 40;
+// var noteBio1 = 40;
+// var noteHistory1 = 50;
+// var stu2Yas = 2024 - Number(stu2Birth);
+// var stu2Average = (noteMath1 + noteBio1 + noteHistory1) / 3;
+// var kecib1 = stu2Average >= 50;
+// console.log("Yas: "+stu2Yas+" Ortalama: " + stu2Average + " Kecib: "+ kecib1);
 
-var stu2Name = "Kamal Bayram";
-var stu2Birth = "1997";
-var noteMath1 = 40;
-var noteBio1 = 40;
-var noteHistory1 = 50;
-var stu2Yas = 2024 - Number(stu2Birth);
-var stu2Average = (noteMath1 + noteBio1 + noteHistory1) / 3;
-var kecib1 = stu2Average >= 50;
-console.log("Yas: "+stu2Yas+" Ortalama: " + stu2Average + " Kecib: "+ kecib1);
+
+// var num = 60;
+// if(num >= 10 && num <= 50)
+// {console.log("10 ve 50 arasindadir");}
+// else{console.log("deyil");}
+
+
+// var num = 2;
+
+// if(num >=1 && num <= 9)
+// {
+//     console.log("tek ve pozitiv reqem");
+// }
+
+var x = 6 , y = 5, z = 3;
+
+
+if(x >= y && x >= z)
+{
+    console.log("x is max");
+}
+else if(y >= x && y >= z)
+{
+    console.log("y is MAX");
+}
+else{
+    console.log("z IS MAX");
+}
+
 
 
